190532630f3f136c469663669cd701f59024ec43
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-16 13:41:06 +0530

    Add files via upload

---
+++ Looping.py
@@ -0,0 +1,18 @@
+name="Rahul"
+for i in name:
+    if (name!='l'):
+        print(i)
+
+lists=['ram','raghav','sonu','pink','solver','coders']
+for i in lists:
+    print(i)
+
+for num in range(1,15): # 1 including 15 excluding
+    print(num)
+    
+k=2
+for i in range(1,11): # print table
+    print(k, "X", i, "=", k*i)
+
+for l in range(1,100,2): #three parameters(starting,ending,major difference)
+    print(l)
