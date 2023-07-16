399a27c8feea7c2dee023d4cff5329ebe3a600f4
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-16 14:22:50 +0530

    Looping.py

--- Looping.py
+++ Looping.py
@@ -16,3 +16,15 @@ for i in range(1,11): # print table
 
 for l in range(1,100,2): #three parameters(starting,ending,major difference)
     print(l)
+    
+# while loop
+j=0
+while(j<=100):
+    print(j+1)
+    j=j+1
+
+m=100
+while(m>=0):
+    print(m)
+    m=m-1
+    
